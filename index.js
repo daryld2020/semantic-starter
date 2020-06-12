74d26df11382f3520323bbe8ce56f2c4bb42b6cf
wsadmin-dlal <wsadmin-dlal@cccco.edu>
2020-06-11 21:10:40 -0700

    my final project

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-window.onload = function() {
+window.onload = function () {
   // necessary for dropdown menus to function properly:
   // https://stackoverflow.com/a/25347431
   $('.ui.dropdown').dropdown();
-};
+
+
+  let button = document.querySelector("#calc")
+  button.addEventListener("click", function () {
+
+    //entry from 1stbox
+    var AA = document.getElementById("input1id").value;
+    //entry from 2ndbox
+    var BB = document.getElementById("input2id").value;
+    //Now put it
+    var sum = (AA) + " " + (BB);
+    //Result will post on new clean screen
+    document.write(sum);
+
+  })
+
+} 
